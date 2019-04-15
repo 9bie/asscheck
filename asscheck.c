6be100a199100d5f19da5fd4d0c1de11e8b64489
bakabie <blackguwc@163.com>
2019-04-16 00:50:04 +0800

    emmm

--- asscheck.c
+++ asscheck.c
@@ -1,9 +1,38 @@
 // 检测器核心文件
 #include <stdio.h>
-
-
-int int main(int argc, char const *argv[])
+#include<windows.h>
+#define MAX_SIZE 255
+struct variable
 {
-    /* code */
+    char name;
+    char size;
+    
+}
+struct chunk
+{
+    int pStack ;//栈平衡指针，用于计算push和pop是否统一
+    int size;
+};
+BOOL IsFileExits(char *path){
+    printf("Precompiled:%s\n",path);
+    WIN32_FIND_DATA  FindFileData;
+    HANDLE hFind;
+    hFind = FindFirstFile(path, &FindFileData);
+    if (hFind == INVALID_HANDLE_VALUE){
+        printf ("Invalid File Handle. Get Last Error reports %d ", GetLastError ());
+    }else{
+        printf ("The first file found is %s ", FindFileData.cFileName);
+        FindClose(hFind);
+    }
+}
+
+int  main(int argc, char *argv[])
+{
+    if (argc == 1){
+        printf("usage: asscheck.exe [path]\n    Precompiled the code.\n");
+    }else {
+        
+        IsFileExits(argv[1]);
+    }
     return 0;
 }
